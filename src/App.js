5c0c71f66baf8e4127f981b4b198ee741af8cf37
joeychun <joey.s.chun@gmail.com>
2023-03-21 01:35:41 +0900

    Updated '/' Navigate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/header/header';
 import Soccer from './sports/soccer/soccer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,6 +9,10 @@ function App() {
       <div className="App" style={{ backgroundColor: "black" }}>
         <Header />
         <Routes>
+          <Route
+            path='/'
+            exact element={<Navigate to='/soccer' />}
+          />
           <Route path='/soccer' exact element={<Soccer />} />
           <Route path='/soccer/:id' exact element={<Soccer />} />
         </Routes>
